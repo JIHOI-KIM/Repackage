4f02b3969b355dc282b6b7eaba615f3c95efd3b3
JIHOI-KIM <57209681+JIHOI-KIM@users.noreply.github.com>
2021-04-21 16:08:41 +0900

    210421 1607
    dummy so file and source
    (ARMGCC -shared -o libdummy.so -fPIC dummy.c

---
+++ dummy.c
@@ -0,0 +1,29 @@
+int DUMMY_FUNC1(void)
+{
+	return 1;
+}
+
+int DUMMY_FUNC2(void)
+{
+	return 2;
+}
+
+int DUMMY_FUNC3(void)
+{
+	return 3;
+}
+
+int DUMMY_FUNC4(void)
+{
+	return 4;
+}
+
+int DUMMY_FUNC5(void)
+{
+	return 5;
+}
+
+int DUMMY_FUNC6(void)
+{
+	return 6;
+}
